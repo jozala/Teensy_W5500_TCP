694a66add1814d9bad3f26ed5ba88eddf7705016
Mariusz <mariusz@aetas.pl>
2020-07-03 09:31:13 +0200

    Initial commit

---
+++ include/MasterConfig.h
@@ -0,0 +1,10 @@
+#define ETHERNET_MAC            "64:BE:E3:D8:76:9E" // Ethernet MAC address (have to be unique between devices in the same network)
+#define ETHERNET_IP             "10.0.88.244"       // IP address of RoomHub when on Ethernet connection
+
+#define ETHERNET_RESET_PIN      2      // Teensy pin where reset pin from W5500 is connected
+#define ETHERNET_CS_PIN         10     // Teensy pin where CS pin from W5500 is connected
+
+#define TCP_HOSTNAME           "10.0.88.87"
+#define TCP_PORT               6789
+
+#define TCP_PUBLISH_INTERVAL_MS   1000
